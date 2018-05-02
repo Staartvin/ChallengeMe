9c76e08f539ac106c6eb23922a8175b41298f2cb
Staartvin <mijnleraar@msn.com>
2018-05-02 22:03:40 +0200

    Create ChallengeScheduler

---
+++ src/plugins/challengeme/scheduler/ChallengeScheduler.java
@@ -0,0 +1,14 @@
+package plugins.challengeme.scheduler;
+
+import plugins.challengeme.ChallengeMe;
+
+public class ChallengeScheduler {
+
+    private ChallengeMe plugin;
+
+    public ChallengeScheduler(ChallengeMe instance) {
+        this.plugin = instance;
+    }
+
+
+}
